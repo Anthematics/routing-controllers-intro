a429c3a5d08c663e3926fe5cd6e52e7a5408067f
Anthematics <jasonharder2@gmail.com>
2017-08-30 16:38:42 -0400

    added secret route and password (works)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,8 +11,19 @@ class PagesController < ApplicationController
 	end
 
 	def secrets
-	end
 
+		if params[:magic_word] == "octopus"
+			puts "right"
+		else
+			redirect_to "/welcome"
+			flash[:alert] = "tut-tut that's not the magic word so you shall not pass"
+		end
+	end
+# Our goal is to make it so that
+#  only the correct magic word will allow you to see the secrets page.
+#  You can do this by putting a redirect_to inside an if statement that compares params[:magic_word]
+	#  (the wildcard from the URL) to a string that contains the magic word of your choosing and see if they match.
+	#  It's up to you what other URL you want to redirect them to in the case that the magic words don't match.?
 	def set_kitten_url
 		requested_size = params[:size]
 		@kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"

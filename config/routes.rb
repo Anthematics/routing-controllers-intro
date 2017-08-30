d5cc71e752f3ba8b475ae1b1966a271082584f23
Anthematics <jasonharder2@gmail.com>
2017-08-30 15:17:45 -0400

    V.0.11 Now about me page connects (wrote first connecting route method successfully)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
 get '/welcome' => 'pages#welcome'  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 get '/' => 'pages#welcome'
+
+get'/about' => 'pages#about' #(first routing method written)
+
 end
